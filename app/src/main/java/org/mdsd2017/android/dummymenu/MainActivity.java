106c9c35aa0aa56f7540b7a5e307ae6477756323
hilda <hilda.lopez@cornerjob.com>
2017-02-27 09:29:48 +0100

    Temp

--- app/src/main/java/org/mdsd2017/android/dummymenu/MainActivity.java
+++ app/src/main/java/org/mdsd2017/android/dummymenu/MainActivity.java
@@ -2,10 +2,14 @@ package org.mdsd2017.android.dummymenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "In-MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item_add:
+                Log.i(MainActivity.TAG, "Add option");
+                break;
+            case R.id.item_like:
+                Log.i(MainActivity.TAG, "Like option");
+                break;
+            case R.id.item_dislike:
+                Log.i(MainActivity.TAG, "Dislike option");
+                break;
+            default:
+                Log.i(MainActivity.TAG, "Other option");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
